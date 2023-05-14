2448cafcf91145677a3eec1ba7ecc074dacb29ee
hieuvv <hieuvv@sapo.vn>
2023-05-15 06:03:59 +0700

    update add sapo ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,172 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Card,
+  FormLayout,
+  Frame,
+  Page,
+  PageActions,
+  Select,
+  TextField,
+  Toast,
+} from "@sapo/ui-components";
+import { useState } from "react";
+import { Controller, SubmitErrorHandler, useForm } from "react-hook-form";
+import { toString } from "lodash-es";
+
+type FormValues = {
+  name: string;
+  age: string;
+  sex: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [toast, setToast] = useState(false);
+  const {
+    control,
+    handleSubmit,
+    reset,
+    watch,
+    trigger,
+    formState: {
+      isValid,
+      isDirty,
+      errors,
+      isSubmitting: submitting,
+      isSubmitted: submitted,
+    },
+  } = useForm<FormValues>({
+    defaultValues: {
+      name: "",
+      age: "",
+      sex: "",
+    },
+  });
+
+  async function onSubmit(data: FormValues): Promise<void> {
+    reset(data);
+    setToast(true);
+  }
+
+  const onClientError: SubmitErrorHandler<FormValues> = (errors) => {
+    console.log("errors", errors);
+    setToast(true);
+  };
+
+  const submit = handleSubmit(onSubmit, onClientError);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + test</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    <Frame>
+      <Page title="Login">
+        {toast && (
+          <Toast
+            content={isValid ? "Update thành công" : "Fix form "}
+            success={isValid}
+            error={!isValid}
+            onDismiss={() => setToast(false)}
+          />
+        )}
+        {/* {isDirty && (
+        <ContextualSaveBar
+          message="Chỉnh sửa chưa được lưu"
+          saveAction={{
+            content: "Lưu",
+            onAction: submit,
+            loading: submitting,
+            disabled: !isValid,
+          }}
+          discardAction={{
+            content: "Hủy",
+            discardConfirmationModal: true,
+            onAction: reset,
+            disabled: submitting,
+          }}
+        />
+      )} */}
+        <Card sectioned>
+          <div>
+            <p>You enter name: {watch("name")}</p>
+            <p>Form valid: {toString(isValid)}</p>
+            <p>Form dirty: {toString(isDirty)}</p>
+            <p>Form isSubmitting: {toString(submitting)}</p>
+            <p>Form isSubmitted: {toString(submitted)}</p>
+            <p>Form errors: {JSON.stringify(errors)}</p>
+            <FormLayout>
+              <Controller
+                name="name"
+                control={control}
+                rules={{ required: "Enter name" }}
+                render={({
+                  field: { onChange, value, name },
+                  fieldState: { error },
+                }) => (
+                  <TextField
+                    label="Name"
+                    placeholder="Enter name"
+                    onChange={onChange}
+                    value={value}
+                    name={name}
+                    error={error?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="age"
+                control={control}
+                rules={{ required: "Enter age" }}
+                render={({
+                  field: { onChange, value, name },
+                  fieldState: { error },
+                }) => (
+                  <TextField
+                    label="Age"
+                    placeholder="Enter age"
+                    onChange={onChange}
+                    value={value}
+                    name={name}
+                    error={error?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="sex"
+                control={control}
+                rules={{ required: "Enter sex" }}
+                render={({
+                  field: { onChange, value, name },
+                  fieldState: { error },
+                }) => (
+                  <Select
+                    label="Sex"
+                    options={["male", "female"]}
+                    placeholder="Select sex"
+                    onChange={onChange}
+                    value={value}
+                    name={name}
+                    error={error?.message}
+                  />
+                )}
+              />
+            </FormLayout>
+          </div>
+        </Card>
+        <PageActions
+          secondaryActions={[
+            {
+              content: "Reset",
+              onAction: reset,
+            },
+            {
+              content: "Validate",
+              onAction: trigger,
+            },
+          ]}
+          primaryAction={{
+            content: "Submit",
+            onAction: submit,
+          }}
+        />
+      </Page>
+    </Frame>
+  );
 }
 
-export default App
+export default App;
