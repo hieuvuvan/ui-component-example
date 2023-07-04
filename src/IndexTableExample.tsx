68c7350bae9273c703178abe7dd704cb0fe9cda8
hieuvv <hieuvv@sapo.vn>
2023-07-04 16:13:14 +0000

    IndexTable example

---
+++ src/IndexTableExample.tsx
@@ -0,0 +1,121 @@
+import {
+  Badge,
+  Card,
+  IndexTable,
+  IndexTableProps,
+  Text,
+  useIndexResourceState,
+} from "@sapo/ui-components";
+
+const orders = [
+  {
+    id: "1020",
+    order: "#1020",
+    date: "13/06/2023 16:36",
+    customer: "Tuyến",
+    total: "100.000đ",
+    paymentStatus: <Badge progress="complete">Đã thanh toán</Badge>,
+    fulfillmentStatus: (
+      <Badge progress="complete" status="success">
+        Đã xử lý
+      </Badge>
+    ),
+  },
+  {
+    id: "1019",
+    order: "#1019",
+    date: "13/06/2023 14:53",
+    customer: "Anh Nguyễn Đức",
+    total: "2.800.000₫",
+    paymentStatus: (
+      <Badge progress="partiallyComplete" status="highlight">
+        Thanh toán một phần
+      </Badge>
+    ),
+    fulfillmentStatus: (
+      <Badge progress="partiallyComplete" status="highlight">
+        Đang xử lý
+      </Badge>
+    ),
+  },
+  {
+    id: "1018",
+    order: "#1018",
+    date: "14/06/2023 09:59	",
+    customer: "	hà nguyễn văn",
+    total: "200.000đ",
+    paymentStatus: (
+      <Badge progress="incomplete" status="warning">
+        Chưa thanh toán
+      </Badge>
+    ),
+    fulfillmentStatus: (
+      <Badge progress="incomplete" status="critical">
+        Chưa xử lý
+      </Badge>
+    ),
+  },
+];
+
+const defaultOrderHeaders: IndexTableProps["headings"] = [
+  {
+    title: "Mã đơn hàng",
+  },
+  {
+    title: "Ngày tạo đơn",
+  },
+  {
+    title: "Tên khách hàng",
+  },
+  {
+    title: "Thành tiền",
+    alignment: "end",
+  },
+  {
+    title: "Trạng thái thanh toán",
+  },
+  {
+    title: "Trạng thái vận chuyển",
+  },
+];
+
+export function IndexTableExample() {
+  const { selectedResources, handleSelectionChange } =
+    useIndexResourceState(orders);
+  const rowMarkup = orders.map(
+    (
+      { id, order, date, customer, total, paymentStatus, fulfillmentStatus },
+      index
+    ) => (
+      <IndexTable.Row
+        id={id}
+        key={id}
+        selected={selectedResources.includes(id)}
+        position={index}
+      >
+        <IndexTable.Cell>
+          <Text variant="bodyMd" fontWeight="bold" as="span">
+            {order}
+          </Text>
+        </IndexTable.Cell>
+        <IndexTable.Cell>{date}</IndexTable.Cell>
+        <IndexTable.Cell>{customer}</IndexTable.Cell>
+        <IndexTable.Cell>{total}</IndexTable.Cell>
+        <IndexTable.Cell>{paymentStatus}</IndexTable.Cell>
+        <IndexTable.Cell>{fulfillmentStatus}</IndexTable.Cell>
+      </IndexTable.Row>
+    )
+  );
+  return (
+    <Card>
+      <IndexTable
+        itemCount={orders.length}
+        selectedItemsCount={selectedResources.length}
+        onSelectionChange={handleSelectionChange}
+        headings={defaultOrderHeaders}
+      >
+        {rowMarkup}
+      </IndexTable>
+    </Card>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { AppProvider } from "@sapo/ui-components";
 import trans from "@sapo/ui-components/locales/vi.json";
+import { IndexTableExample } from "./IndexTableExample.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AppProvider i18n={trans}>
-      <App />
+      <IndexTableExample />
     </AppProvider>
   </React.StrictMode>
 );
