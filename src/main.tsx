67b8420811d9cefcd009788fb9958d8eabea5a89
hieuvv <hieuvv@sapo.vn>
2023-07-04 22:30:11 +0700

    using npm

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { AppProvider } from '@sapo/ui-components'
+import trans from "@sapo/ui-components/locales/vi.json";
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AppProvider>
+    <AppProvider i18n={trans}>
       <App />
     </AppProvider>
   </React.StrictMode>,
